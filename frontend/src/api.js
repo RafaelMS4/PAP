efc98c6a0c974275646e13bd3b9ffa2ba174e30f
Rafael?? <rafael.m.santos2008@gmail.com>
2026-01-12 18:08:59 +0000

    Aula

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 
 const api = axios.create({
   baseURL: API_URL,
@@ -33,8 +33,8 @@ api.interceptors.response.use(
 export const authAPI = {
   login: (username, password) => api.post('/auth/login', { username, password }),
   getCurrentUser: () => api.get('/auth/me'),
-  getUsers: () => api.get('/auth/users'),
-  createUser: (userData) => api.post('/auth/users', userData)
+  getUsers: () => api.get('/users/getUsers'),
+  createUser: (userData) => api.post('/users/createUser', userData)
 };
 
 export default api;
